f775335a83d88404660601124fee67992e8a1c2b
NikUlka <m_y@mail.ru>
2023-05-18 10:46:33 +0300

    Update [hw]02_task_string.py

--- Module4/home_work/01_string/[hw]02_task_string.py
+++ Module4/home_work/01_string/[hw]02_task_string.py
@@ -1,3 +1,19 @@
 # Дана строка состоящая из слов, все слова разделены ровно одним пробелом. Знаки препинания отсутствуют.
 # Найдите первое слово, начинающееся на букву "б"
 # если слова на эту букву нет, выведите "слов на б нет"
+text = "Скоро лето"
+#text = "Будет лето"
+#text = "Лето будет"
+
+start_word = text.lower().find(" б") + 1
+if text.find(" ", start_word) == -1:
+    end_word = len(text)
+else:
+    end_word = text.find(" ", start_word + 1)
+
+if text[0].lower() == 'б':
+    print(text[0:text.find(" ")])
+elif text[start_word].lower() == 'б':
+    print(text[start_word:end_word])
+else:
+    print("слов на б нет")
