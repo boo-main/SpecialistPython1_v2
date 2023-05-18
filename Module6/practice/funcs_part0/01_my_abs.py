64842e6a9b89d61a15af96da78cf983898d6accc
NikUlka <m_y@mail.ru>
2023-05-18 12:27:59 +0300

    Update 01_my_abs.py

--- Module6/practice/funcs_part0/01_my_abs.py
+++ Module6/practice/funcs_part0/01_my_abs.py
@@ -1,7 +1,9 @@
-def my_abs(...):
-    ...
-    return ...
-
+def my_abs(n):
+    if n < 0:
+        result = -1 * n
+    else:
+        result = n
+    return result
 
 print(my_abs(-5))
 print(my_abs(5))
